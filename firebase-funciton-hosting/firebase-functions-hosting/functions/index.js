f33061906bc739b86a84a81c37142b66cc2f15b4
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-04-10 22:59:31 -0700

    chore: add project

--- firebase-funciton-hosting/firebase-functions-hosting/functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const express = require('express');
-
-const app = express();
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
-})
-
-exports.app = functions.https.onRequest(app);
